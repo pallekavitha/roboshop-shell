8a0b4b969c45e65bef73d453ee109d271d89c008
pallekavitha <kavithapallewar007@gmail.com>
2023-09-04 00:06:04 +0530

    created mysql

---
+++ shipping.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+DATE=$(date +%F)
+LOGSDIR=/tmp
+# /home/centos/shellscript-logs/script-name-date.log
+SCRIPT_NAME=$0
+LOGFILE=$LOGSDIR/$0-$DATE.log
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+if [ $USERID -ne 0 ];
+then
+    echo -e "$R ERROR:: Please run this script with root access $N"
+    exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ];
+    then
+        echo -e "$2 ... $R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
+}
+
+yum install maven -y
+
+useradd roboshop
+
+mkdir /app
+
+curl -L -o /tmp/shipping.zip https://roboshop-builds.s3.amazonaws.com/shipping.zip
+
+cd /app
+
+unzip /tmp/shipping.zip
+
+mvn clean package
+
+mv target/shipping-1.0.jar shipping.jar
+
+/etc/systemd/system/shipping.service
+
+systemctl daemon-reload
+
+systemctl enable shipping 
+
+systemctl start shipping
+
+yum install mysql -y 
+
+mysql -h <MYSQL-SERVER-IPADDRESS> -uroot -pRoboShop@1 < /app/schema/shipping.sql 
+
+systemctl restart shipping
