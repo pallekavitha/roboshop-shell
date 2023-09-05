8ccdf5692e604dd5faa6adee99916826a3a3b3d0
pallekavitha <kavithapallewar007@gmail.com>
2023-09-05 13:39:50 +0530

    added servers scrpit

--- create-ec2.sh
+++ create-ec2.sh
@@ -2,7 +2,7 @@
 
 NAMES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
 INSTANCE_TYPE=""
-IMAGE_ID=ami-03265a0778a880afb
+IMAGE_ID=ami-081609eef2e3cc958
 SECURITY_GROUP_ID=sg-0be4b23bfc8adb5af
 
 # if mysql or mongodb instance_type should be t3.medium , for all others it is t2.micro
