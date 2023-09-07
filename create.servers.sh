2f3cd2ed51d3827456733367214e9990fc5323a0
pallekavitha <pallewarkavitha007@gmail.com>
2023-09-07 19:19:12 +0530

    ec2 server file

---
+++ create.servers.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+NAMES=$@
+INSTANCE_TYPE=""
+IMAGE_ID=ami-081609eef2e3cc958
+SECURITY_GROUP_ID=sg-0be4b23bfc8adb5af
+DOMAIN_NAME=joindevops.live
+
+# if mysql or mongodb instance_type should be t3.medium , for all others it is t2.micro
+
+for i in $@
+do 
+    if [[ $i == "mongodb" || $i == "mysql" ]]
+    then
+        INSTANCE_TYPE="t3.medium"
+    else
+        INSTANCE_TYPE="t2.micro"
+    fi
+    echo "creating $i instance"
+    IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID  --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
+    echo "created $i instance: $IP_ADDRESS"
+
+aws route53 change-resource-record-sets --hosted-zone-id Z00055312F8QWOU888ABK --change-batch '
+{
+            "Changes": [{
+            "Action": "CREATE",
+                        "ResourceRecordSet": {
+                            "Name": "'$i.$DOMAIN_NAME'",
+                            "Type": "A",
+                            "TTL": 300,
+                            "ResourceRecords": [{ "Value": "'$IP_ADDRESS'"}]
+                        }}]
+}
+'
+done
