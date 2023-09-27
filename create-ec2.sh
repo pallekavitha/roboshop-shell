9d90bd0476f014478399523bf8dfbaa3c5ea616c
pallekavitha <pallewarkavitha007@gmail.com>
2023-09-27 12:48:38 +0530

    edited file

--- create-ec2.sh
+++ create-ec2.sh
@@ -2,8 +2,8 @@
 
 NAMES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
 INSTANCE_TYPE=""
-IMAGE_ID=ami-081609eef2e3cc958
-SECURITY_GROUP_ID=sg-0be4b23bfc8adb5af
+IMAGE_ID=ami-03265a0778a880afb
+SECURITY_GROUP_ID=sg-0ecfa77aceccce8fd
 DOMAIN_NAME=joindevops.live
 
 # if mysql or mongodb instance_type should be t3.medium , for all others it is t2.micro
